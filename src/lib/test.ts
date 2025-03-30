d5e2867fa1bed20dfb838a869da38c62f4df306f
wooram <rla7360@gmail.com>
2025-03-30 13:32:45 +0900

    fix : 타입오류 제거

--- src/lib/test.ts
+++
@@ -1,9 +0,0 @@
-import { sendPublicRequestWithToast } from "./sendApi";
-
-export const getApi = async () => {
-    const response = await sendPublicRequestWithToast({
-        url: '/center/list/idName',
-        method: 'GET'
-    })
-    return response.data
-}
