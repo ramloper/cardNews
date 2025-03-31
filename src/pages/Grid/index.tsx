ff8f431566066300279c02f63207f6209a35fd44
ramloper <rla7360@gmail.com>
2025-03-31 11:06:15 +0900

    fix : 그리드 화면 타이틀 수정도 패딩탑 추가

--- src/pages/Grid/index.tsx
+++ src/pages/Grid/index.tsx
@@ -1,69 +1,85 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
-import Skeleton from "react-loading-skeleton";
-import Thumbnail from "../../components/Thumbnail";
-import { myToast } from "../../lib/alert";
-import { getTitle, patchTitle } from "../../lib/notice";
-import { getPosts } from "../../lib/post";
-import { PostType } from "../../types/post";
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useState } from "react"
+import Skeleton from "react-loading-skeleton"
+import Thumbnail from "../../components/Thumbnail"
+import { myToast } from "../../lib/alert"
+import { getTitle, patchTitle } from "../../lib/notice"
+import { getPosts } from "../../lib/post"
+import { PostType } from "../../types/post"
 export default function Grid() {
-    const queryClient = useQueryClient();
-    const admin = localStorage.getItem('admin');
+    const queryClient = useQueryClient()
+    const admin = localStorage.getItem("admin")
 
-    const { data: posts, isLoading, error } = useQuery({
-        queryKey: ['posts'],
+    const {
+        data: posts,
+        isLoading,
+        error,
+    } = useQuery({
+        queryKey: ["posts"],
         queryFn: getPosts,
-    });
-    const { data: title, isLoading: titleLoading, error: titleError } = useQuery({
-        queryKey: ['title'],
+    })
+    const {
+        data: title,
+        isLoading: titleLoading,
+        error: titleError,
+    } = useQuery({
+        queryKey: ["title"],
         queryFn: getTitle,
     })
-    const [patchTitleStatus, setPatchTitleStatus] = useState(false);
-    const [titleValue, setTitleValue] = useState<string>(title?.title || "");
+    const [patchTitleStatus, setPatchTitleStatus] = useState(false)
+    const [titleValue, setTitleValue] = useState<string>(title?.title || "")
     const onClickPatchTitleValue = () => {
         if (patchTitleStatus) {
-            patchTitle(titleValue).then(() => {
-                myToast('제목 수정 성공', 'success');
-                setPatchTitleStatus(false);
-                queryClient.invalidateQueries({ queryKey: ['title'] });
-            }).catch((error) => {
-                myToast(error.response.data.data.value, 'error');
-            })
+            patchTitle(titleValue)
+                .then(() => {
+                    myToast("제목 수정 성공", "success")
+                    setPatchTitleStatus(false)
+                    queryClient.invalidateQueries({ queryKey: ["title"] })
+                })
+                .catch((error) => {
+                    myToast(error.response.data.data.value, "error")
+                })
         } else {
             setPatchTitleStatus(true)
         }
     }
     if (isLoading || titleLoading) {
-        return (<div className="max-w-[935px] mx-auto py-4 px-2">
-            <div className="w-full xl:w-[600px] h-full">
-                <Skeleton circle width={40} height={40} className="mr-3" />
+        return (
+            <div className='max-w-[935px] mx-auto py-4 px-2'>
+                <div className='w-full xl:w-[600px] h-full'>
+                    <Skeleton circle width={40} height={40} className='mr-3' />
+                </div>
             </div>
-        </div>);
+        )
     }
     if (error || titleError) {
-        return <div className="flex justify-center p-4">Error loading posts</div>;
+        return <div className='flex justify-center p-4'>Error loading posts</div>
     }
     return (
-        <div className="max-w-[1000px] mx-auto px-4">
-            <div className="flex flex-row items-center justify-center gap-16">
+        <div className='max-w-[1000px] mx-auto px-4 py-8'>
+            <div className='flex flex-row items-center justify-center gap-16'>
                 {admin !== null && patchTitleStatus ? (
-                    <input type="text" placeholder="제목을 입력해주세요" className="w-[200px] h-[40px] rounded-md border-2 border-gray-300 p-2" value={titleValue} onChange={(e) => setTitleValue(e.target.value)} />
+                    <input
+                        type='text'
+                        placeholder='제목을 입력해주세요'
+                        className='w-[200px] h-[40px] rounded-md border-2 border-gray-300 p-2'
+                        value={titleValue}
+                        onChange={(e) => setTitleValue(e.target.value)}
+                    />
                 ) : (
-                    <h1 className="text-[26px] font-bold py-8 dark:text-white text-center">
-                        {title?.title}
-                    </h1>
+                    <h1 className='text-[26px] font-bold dark:text-white text-center'>{title?.title}</h1>
                 )}
                 {admin !== null && (
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={onClickPatchTitleValue}>
+                    <button className='bg-blue-500 text-white px-4 py-2 rounded-md' onClick={onClickPatchTitleValue}>
                         제목 수정
                     </button>
                 )}
             </div>
-            <div className="grid grid-cols-3 gap-1">
+            <div className='grid grid-cols-3 gap-1'>
                 {posts.map((post: PostType) => (
                     <Thumbnail key={post.boardId} post={post} />
                 ))}
             </div>
         </div>
-    );
+    )
 }
